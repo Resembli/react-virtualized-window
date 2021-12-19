8fb712b2f1f8d280f6fc20abb1f98c234f7861a3
Lee Beydoun <leebeydoun@outlook.com>
2021-12-19 13:52:12 +0000

    [WINDOW]: added row virtualisation handling

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,5 +1,9 @@
 import { Window } from "@resembli/le-window"
 
+const listItems = Array(1000)
+  .fill(0)
+  .map((_, i) => i)
+
 export const App = () => {
-  return <Window width={800} height={800} rowHeight={20} />
+  return <Window width={800} height={800} rowHeight={20} data={listItems} />
 }
