84aeab7db902f55193de228cc24b8c90fb52c5e2
Lee Beydoun <leebeydoun@outlook.com>
2021-12-21 12:20:51 +0000

    [WINDOW]: changed scrolling logic to use a sticky position implementation

--- packages/le-window/src/Window.tsx
+++ packages/le-window/src/Window.tsx
@@ -1,4 +1,7 @@
-import { useEffect, useMemo, useReducer, useRef, useState } from "react"
+import { useMemo, useRef } from "react"
+
+import { useWindowDimensions } from "./useWindowDimensions"
+import { useWindowScroll } from "./useWindowScroll"
 
 export interface WindowProps<T> {
   rowHeight: number
@@ -11,124 +14,49 @@ export const Window = <T extends Record<string, unknown>>({
   data,
   ItemComponent,
 }: WindowProps<T>) => {
-  const overlayRef = useRef<HTMLDivElement>(null)
   const windowRef = useRef<HTMLDivElement>(null)
-  const contentRef = useRef<HTMLDivElement>(null)
-  const topShiftRef = useRef<HTMLDivElement>(null)
-  const windowHeightRef = useRef(0)
-  const windowWidthRef = useRef(0)
-
-  const [offset, setOffset] = useState(0)
-  const [, forceUpdate] = useReducer((x) => (x + 1) % 10, 0)
 
   const innerHeight = useMemo(() => rowHeight * data.length, [data.length, rowHeight])
 
-  useEffect(() => {
-    const handleScroll = (event: Event) => {
-      if (!topShiftRef.current || !contentRef.current) return
-
-      const target = event.currentTarget as HTMLDivElement
-
-      const newOffset = target.scrollTop
-
-      setOffset(newOffset)
-      // topShiftRef.current.style.height = `${newOffset}px`
-      contentRef.current.style.transform = `translate3d(0px, -${newOffset}px, 0px)`
-    }
-
-    const handleWheel = (event: WheelEvent) => {
-      if (!contentRef.current || !overlayRef.current || !topShiftRef.current) return
-
-      const newOffset = Math.min(
-        Math.max(0, offset + event.deltaY),
-        innerHeight - windowHeightRef.current,
-      )
-      setOffset(newOffset)
-
-      overlayRef.current.scrollTop = newOffset
-      contentRef.current.style.transform = `translate3d(0px, -${newOffset}px, 0px)`
-    }
-
-    const overlayNode = overlayRef.current
-    const contentNode = contentRef.current
-
-    overlayNode?.addEventListener("scroll", handleScroll)
-    contentNode?.addEventListener("wheel", handleWheel)
-
-    return () => {
-      overlayNode?.removeEventListener("scroll", handleScroll)
-      contentNode?.removeEventListener("wheel", handleWheel)
-    }
-  }, [innerHeight, offset])
-
-  useEffect(() => {
-    if (!overlayRef.current) return
-
-    const parentWidth = overlayRef.current.parentElement?.clientWidth
-    const parentHeight = overlayRef.current.parentElement?.clientHeight
-
-    if (!parentWidth || !parentHeight)
-      throw new Error("Expected overlay div to have a parent element")
-
-    overlayRef.current.style.width = `${parentWidth}px`
-    overlayRef.current.style.height = `${parentHeight}px`
-  }, [])
-
-  useEffect(() => {
-    if (!overlayRef.current || !windowRef.current) return
-
-    windowWidthRef.current = overlayRef.current.clientWidth
-    windowHeightRef.current = overlayRef.current.clientHeight
-
-    windowRef.current.style.width = `${windowWidthRef.current}px`
-    windowRef.current.style.height = `${windowHeightRef.current}px`
-
-    // After all effect calculations are complete, we need to force a re-render as the
-    // div for the list will have changed height.
-    forceUpdate()
-  }, [])
+  const [, height] = useWindowDimensions(windowRef)
+  const [offset, onScroll] = useWindowScroll()
 
   // TODO: These calculations are for fixed size row heights. It will need to adjusted for variable size row heights.
-  const itemsPerWindow = Math.ceil(windowHeightRef.current / rowHeight)
+  const itemsPerWindow = Math.ceil(height / rowHeight)
 
   const start = Math.floor(offset / rowHeight)
   const end = itemsPerWindow + start
 
-  const topShiftHeight = start * rowHeight
+  // Prevents an issue where we scroll to the bottom, then scrolling a little up applies a translation
+  // moving the div a little higher than it should be.
+  const translationOffset = innerHeight - offset - height < rowHeight ? 0 : -offset % rowHeight
 
   return (
-    <div style={{ position: "relative", width: "100%", height: "100%" }}>
-      <div
-        ref={overlayRef}
-        style={{
-          position: "relative",
-          boxSizing: "border-box",
-          overflow: "auto",
-        }}
-      >
-        <div style={{ height: innerHeight }}></div>
-      </div>
-      <div
-        ref={windowRef}
-        style={{
-          position: "absolute",
-          top: 0,
-          overflow: "hidden",
-        }}
-        onClick={() => console.log("I was clicked")}
-      >
-        <div ref={contentRef} style={{ width: "100%", height: "100%" }}>
-          <div ref={topShiftRef} style={{ height: topShiftHeight }}></div>
-          {data.slice(start, end + 1).map((d, i) => {
-            return (
-              <div
-                key={i}
-                style={{ height: rowHeight, maxHeight: rowHeight, minHeight: rowHeight }}
-              >
-                <ItemComponent {...d} />
-              </div>
-            )
-          })}
+    <div
+      ref={windowRef}
+      onScroll={onScroll}
+      style={{
+        height: "100%",
+        width: "100%",
+        position: "relative",
+        background: "red",
+        overflow: "auto",
+      }}
+    >
+      <div style={{ height: innerHeight }}>
+        <div style={{ position: "sticky", top: 0, bottom: 0, display: "block" }}>
+          <div style={{ transform: `translate3d(0, ${translationOffset}px, 0)` }}>
+            {data.slice(start, end + 1).map((d, i) => {
+              return (
+                <div
+                  key={i}
+                  style={{ height: rowHeight, maxHeight: rowHeight, minHeight: rowHeight }}
+                >
+                  <ItemComponent {...d} />
+                </div>
+              )
+            })}
+          </div>
         </div>
       </div>
     </div>
