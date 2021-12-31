750e78e72556182d9824a13bfd91e1d2632bf2a7
Lee Beydoun <leebeydoun@outlook.com>
2021-12-31 15:07:06 +0000

    [DOCS]: added hero background, switched to png feature image, and added padding back.

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext"
 import Layout from "@theme/Layout"
+import clsx from "clsx"
 
 import HomepageFeatures from "../components/HomepageFeatures"
 import styles from "./index.module.css"
@@ -7,7 +8,7 @@ import styles from "./index.module.css"
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext()
   return (
-    <header className={styles.heroBanner}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
