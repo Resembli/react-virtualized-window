7901653ea66492ee76c73eda2de48e6df57e2a7f
Lee <56801187+leebeydoun@users.noreply.github.com>
2022-01-06 19:58:06 +0000

    [DOCS]: added fixed list suspense example. (#45)

--- docs/src/theme/ReactLiveScope/index.ts
+++ docs/src/theme/ReactLiveScope/index.ts
@@ -1,10 +1,12 @@
 import React from "react"
+import { BeatLoader } from "react-spinners"
 
 import { List } from "@resembli/le-window"
 
 const ReactLiveScope = {
   React,
   List,
+  BeatLoader,
   ...React,
 }
 
