17121a941af179a253918b50d68ee547b9e97afc
Lee <56801187+leebeydoun@users.noreply.github.com>
2022-01-08 12:33:58 +0000

    [DOCS]: added variable widths example and cleaned up docs. (#51)

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -11,8 +11,6 @@ const sidebars = {
         "le-window/list",
         "le-window/list-horizontal",
         "le-window/grid",
-        "le-window/free-form",
-        "le-window/infinite-scroller",
         {
           type: "category",
           label: "Examples",
