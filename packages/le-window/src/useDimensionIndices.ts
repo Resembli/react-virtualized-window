f3da19bf37107af19a8cb4327aa641ac23ec3c0c
Lee <56801187+leebeydoun@users.noreply.github.com>
2022-01-23 19:30:04 +0000

    [WINDOW]: fixed scroll jump, but will need to improve window overscan. (#67)

--- packages/le-window/src/useDimensionIndices.ts
+++ packages/le-window/src/useDimensionIndices.ts
@@ -15,7 +15,9 @@ export const useIndicesForDimensions = ({
     let start = 0
     let runningTotal = 0
 
-    while (start < itemDimensions.length) {
+    const underScan = Math.max(100, windowDimension / 4)
+
+    while (runningTotal < Math.max(0, offset - underScan)) {
       const itemDim = itemDimensions[start]
       if (itemDim + runningTotal > offset) break
 
@@ -23,30 +25,17 @@ export const useIndicesForDimensions = ({
       runningTotal += itemDim
     }
 
-    /**
-     * We over scan a little to allow items to be tab-reachable. We don't want to
-     * over scan too much as that would result in a performance bottleneck.
-     */
-    const overScan = Math.max(0, start - 3)
-    let overScanOffset = 0
-    while (start > overScan) {
-      runningTotal -= itemDimensions[start]
-      overScanOffset += itemDimensions[start]
-      start--
-    }
-
-    const startDim = itemDimensions[start]
     let end = start
-    let endDim = 0
+    let endingTotal = runningTotal
 
-    while (end < itemDimensions.length && endDim < windowDimension + overScanOffset + startDim) {
+    while (endingTotal <= offset + windowDimension) {
       const itemDim = itemDimensions[end]
 
-      endDim += itemDim
+      endingTotal += itemDim
       end++
     }
 
-    return [start, end, runningTotal + overScanOffset / 2]
+    return [start, end, runningTotal]
   }, [itemDimensions, windowDimension, offset])
 
   return [start, end, running] as const
