61dcaf56df500cb51057c34980570da08fc7add5
Lee Beydoun <leebeydoun@outlook.com>
2022-01-29 14:05:43 +0000

    [DEMO]: added routing set-up and a normal list component.

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,9 +1,14 @@
-import { Navbar } from "./Navbar"
+import { Redirect, Route, Switch } from "wouter"
+
+import { Link } from "./components/Link"
+import { NormalList } from "./components/Lists/NormalList"
+import { Navbar } from "./components/Navbar"
+import { SideNav } from "./components/SideNav"
 import { css } from "./theme/theme"
 
 const app = css({
   display: "grid",
-  gridTemplateColumns: "auto",
+  gridTemplateColumns: "auto 1fr",
   gridTemplateRows: "auto 1fr",
   height: "100%",
   backgroundColor: "$appBg",
@@ -13,10 +18,41 @@ const app = css({
 
 const appClass = app()
 
+interface RouteItem {
+  label: string
+  path: string
+  Component: () => JSX.Element
+}
+
 export const App = () => {
+  const routes: RouteItem[] = [
+    { label: "Normal List", path: "/normal-list", Component: NormalList },
+  ]
+
   return (
     <div className={appClass}>
-      <Navbar />
+      <Navbar css={{ gridColumn: "1 / span 2" }} />
+      <SideNav>
+        <ul>
+          {routes.map((route) => (
+            <li key={route.path}>
+              <Link to={route.path}>{route.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </SideNav>
+      <div style={{ margin: 20 }}>
+        <Route path="/">
+          <Redirect to={routes[0].path} />
+        </Route>
+        <Switch>
+          {routes.map((route) => (
+            <Route key={route.path} path={route.path}>
+              <route.Component />
+            </Route>
+          ))}
+        </Switch>
+      </div>
     </div>
   )
 }
