5eb3150c5574e40d6bfc504f96042c31432f0042
Lee Beydoun <leebeydoun@outlook.com>
2022-02-19 13:34:19 +0000

    [ROOT]: added vitebook as a package.

---
+++ book/.vitebook/App.tsx
@@ -0,0 +1,13 @@
+import type { ComponentChildren } from 'preact';
+
+type AppProps = {
+  Component: ComponentChildren;
+};
+
+function App({ Component }: AppProps) {
+  return <Component />;
+}
+
+App.displayName = 'VitebookApp';
+
+export default App;
