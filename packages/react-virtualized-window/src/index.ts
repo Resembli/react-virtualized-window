823c2c347e1f7cfb3a7a62ce0f267a784bd12687
Lee <56801187+leebeydoun@users.noreply.github.com>
2022-02-20 15:09:06 +0000

    [ROOT]: removed the utils package. (#159)

--- packages/react-virtualized-window/src/index.ts
+++ packages/react-virtualized-window/src/index.ts
@@ -1,7 +1,6 @@
 export { List } from "./components/List"
 export { Grid } from "./components/Grid"
 
-export type { GridProps, RenderItem } from "./components/Grid"
 export type { ListProps } from "./components/List"
 export type { VirtualWindowApi } from "./useWindowApi"
-export type { ItemGap, NumberOrPercent } from "./types"
+export type { ItemGap, NumberOrPercent, GridProps, RenderItem, CellMeta } from "./types"
--- packages/utils/src/isBrowser.ts
+++
@@ -1,2 +0,0 @@
-// See https://stackoverflow.com/a/31090240 for an explanation of the below code.
-export const isBrowser = new Function("try {return this===window;}catch(e){ return false;}")()
