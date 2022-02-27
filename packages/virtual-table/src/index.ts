fe9d7326f01e036aefd6f2b531888e9d34d3f18c
Lee Beydoun <leebeydoun@outlook.com>
2022-02-27 11:33:23 +0000

    [VTABLE]: some initial code for a virtualized table.

---
+++ packages/virtual-table/src/index.ts
@@ -0,0 +1,1 @@
+export { VirtualTable } from "./VirtualTable"
--- playgrounds/src/App.tsx
+++ playgrounds/src/App.tsx
@@ -1,77 +1,86 @@
 import { css } from "@stitches/core"
-import { useState } from "react"
+import { useMemo, useState } from "react"
 
-import { Grid } from "@resembli/react-virtualized-window"
+import { VirtualTable } from "@resembli/virtual-table"
 
 const AppCss = css({
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
+  flexDirection: "column",
   width: "100%",
   height: "100%",
   background: "Beige",
 })
 
-const ItemCss = css({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  background: "Azure",
-  variants: {
-    odd: {
-      true: {
-        background: "grey",
-      },
-    },
-    pinned: {
-      true: {
-        background: "Bisque",
-      },
-    },
-  },
-})
+const names = ["Lee", "Bob", "Aya", "Sam"]
 
-const data = Array.from({ length: 1000 }, (_, row) => {
-  return Array.from({ length: 100 }, (_, column) => {
-    return [row, column]
+const generateData = () => {
+  return Array.from({ length: 100 }, (_, i) => {
+    const wins = Math.ceil(Math.random() * 20)
+    const draws = Math.ceil(Math.random() * 20)
+    const losses = Math.ceil(Math.random() * 20)
+    const total = wins + losses + draws
+
+    return [i, names[i % 4], wins, draws, losses, total]
   })
-})
+}
+
+const WINDOW_HEIGHT = 480
+const ITEM_HEIGHT = 20
 
 function App() {
-  const [disableSticky, setStickyDisabled] = useState(false)
+  const data = useMemo(() => generateData(), [])
+
+  const [topOffset, setTopOffset] = useState(0)
+
+  const totalHeight = ITEM_HEIGHT * data.length
+  const startIndex = Math.floor(topOffset / ITEM_HEIGHT)
+  const endIndex = WINDOW_HEIGHT / ITEM_HEIGHT + startIndex + 1
+
+  const runningHeight = startIndex * ITEM_HEIGHT
+  const remainingHeight = totalHeight - runningHeight - WINDOW_HEIGHT
 
   return (
     <div className={AppCss()}>
-      <div>
-        <label>
-          disableStick:
-          <input
-            type="checkbox"
-            checked={disableSticky}
-            onChange={(e) => setStickyDisabled(e.target.checked)}
-          />
-        </label>
-      </div>
-      <Grid
-        defaultColumnWidth={100}
-        defaultRowHeight={100}
-        data={data}
-        width="70%"
-        height="70%"
-        disableSticky={disableSticky}
+      <code>
+        Top Offset: {topOffset}. Start Index: {startIndex}. End Index: {endIndex}.
+      </code>
+
+      <table
+        className="table"
+        cellSpacing={0}
+        onScroll={(e) => {
+          setTopOffset(e.currentTarget.scrollTop)
+        }}
       >
-        {({ data, style, cellMeta }) => (
-          <div
-            style={style}
-            className={ItemCss({
-              odd: (data[0] + data[1]) % 2 === 1,
-              pinned: !!cellMeta.pinnedRow || !!cellMeta.pinnedColumn,
-            })}
-          >
-            {data[0]},{data[1]}
-          </div>
-        )}
-      </Grid>
+        <thead className="thead">
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Wins</th>
+            <th>Draws</th>
+            <th>Losses</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr style={{ height: runningHeight }} />
+          {data.slice(startIndex, endIndex).map((row, i) => {
+            return (
+              <tr key={`${row[0]}-${i + startIndex}`} className="tr">
+                <td>{row[0]}</td>
+                <td>{row[1]}</td>
+                <td>{row[2]}</td>
+                <td>{row[3]}</td>
+                <td>{row[4]}</td>
+                <td>{row[5]}</td>
+              </tr>
+            )
+          })}
+          <tr style={{ height: remainingHeight }} />
+        </tbody>
+      </table>
     </div>
   )
 }
