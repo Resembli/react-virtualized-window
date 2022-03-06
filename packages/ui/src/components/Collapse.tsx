38ff11b71f95d34844f8628d85a8bd5df286249f
Lee Beydoun <leebeydoun@outlook.com>
2022-03-06 16:50:14 +0000

    [UI]: add tree component.

--- packages/ui/src/components/Collapse.tsx
+++ packages/ui/src/components/Collapse.tsx
@@ -3,22 +3,31 @@ import * as React from "react"
 
 export interface CollapseProps {
   open?: boolean
+  className?: string
+  style?: React.CSSProperties
   as?: keyof typeof motion
 }
 
-export function Collapse({ open, children, as = "div" }: React.PropsWithChildren<CollapseProps>) {
+export function Collapse({
+  open,
+  children,
+  as = "div",
+  className,
+  style,
+}: React.PropsWithChildren<CollapseProps>) {
   const MotionElement = motion[as]
 
   return (
     <AnimatePresence initial={false}>
       {open && (
         <MotionElement
+          className={className}
           key="content"
           initial="collapsed"
           animate="open"
           exit="collapsed"
           transition={{ duration: 0.2 }}
-          style={{ overflow: "hidden" }}
+          style={{ overflow: "hidden", ...style }}
           variants={{
             open: { height: "auto" },
             collapsed: { height: 0 },
